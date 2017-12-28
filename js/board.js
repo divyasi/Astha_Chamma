4ce4ec4fa7144b51cd1bb9fc140f18a666d31c07
anju <anjumqureshi000@gmail.com>
2017-12-28 10:25:39 +0530

    draws a 5X5 table

--- js/board.js
+++ js/board.js
@@ -1,25 +1,53 @@
-const generateRow = function(lines) {
-  let row, elementName;
-  for (var count = 0; count < lines; count++) {
-    row = document.createElement("tr");
-    row.id = count+1;
-    console.log(row.id);
-    elementName = document.getElementById("board");
-    generateCols(lines,row);
-    elementName.appendChild(row);
+let idCounter = 1;
+const generateRow = function(noOfCells) {
+  let row = document.createElement("tr");
+  for (var count = 0; count < noOfCells; count++)
+    row.appendChild(generateCell());
+  return row;
+};
+
+const generateCell = function(){
+  let cell = document.createElement('td');
+  cell.id = idCounter++;
+  return cell;
+};
+
+const generateTable = function(noOfRows,noOfCols){
+  let board = document.getElementById('board');
+  for(let count = 0; count < noOfRows; count++){
+    board.appendChild(generateRow(noOfCols));
   }
-}
+};
 
-const generateCols = function(lines,row) {
-  let col;
-  for (var count = 0; count < lines; count++) {
-    col = document.createElement("td");
-    row.appendChild(col);
+const updateCell = function(id,value){
+  document.getElementById(id).innerHTML = value;
+};
+
+const changeClass = function(id,className,value=''){
+  document.getElementById(id).className = className;
+  document.getElementById(id).innerText = value;
+};
+
+const startGame = function(){
+  generateTable(5,5);
+  changeClass('3','safePlace');
+  changeClass('11','safePlace');
+  changeClass('13','home','home');
+  changeClass('15','safePlace');
+  changeClass('23','safePlace');
+  insertCoins(3,'player1');
+  insertCoins(23,'player2');
+};
+
+const insertCoins = function(id,player){
+  let safePlace = document.getElementById(id);
+  for(let count = 0; count < 4; count++){
+    let coin = document.createElement('button');
+    coin.className = player;
+    safePlace.appendChild(coin);
   }
-}
+};
 
-const board = function() {
-  generateRow(5);
-}
 
-window.onload = board;
+
+window.onload = startGame;
